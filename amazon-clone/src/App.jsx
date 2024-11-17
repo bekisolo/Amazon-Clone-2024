9d1ecffb5c2ac606d793734f48284e3abee9194f
bekisolo <bereket.solo7@gmail.com>
2024-11-17 20:22:45 +0300

    category and product section

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header/Header.jsx";
 import CarouselEffect from "./Components/Carousel/CarouselEffect.jsx";
+import Category from "./Components/Category/Category.jsx";
+import Product from "./Components/Product/Product.jsx";
 function App() {
   return (
     <>
       <Header />
       <CarouselEffect />
+      <Category />
+      <Product />
     </>
   );
 }
---
+++ amazon-clone/src/Components/Product/ProductCard.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from 'react'
+import  {Rating}  from '@mui/material'
+import CurrencyFormat from '../CurrencyFormat/CurrencyFormat'
+import classes from "./product.module.css"
+
+const ProductCard = ({ product}) => {
+  const { image, title, id, rating, price } = product;
+
+  return (
+    <div
+      className={`${classes.card_container}`}
+    >
+      <a href=''>
+        <img src={image} alt="" />
+      </a>
+      <div>
+        <h3>{title}</h3>
+        <div className={classes.rating}>
+          {/* rating */}
+          <Rating value={rating.rate} precision={0.1} />
+          {/* price rating count */}
+          <small>{rating.count}</small>
+        </div>
+        <div>
+          {/* price */}
+          <CurrencyFormat amount={price} />
+        </div>
+         
+         {
+
+         <button className={classes.button}>
+          add to cart </button>
+       
+
+         }
+        
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard
